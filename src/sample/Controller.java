9752e08c5b824ed91ea6c05352eb223c2af96b64
Jacek <jacek205@wp.pl>
2018-09-10 22:47:52 +0200

    import class Person. Form done.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField imie;
+
+    @FXML
+    private TextField nazwisko;
+
+    @FXML
+    private TextField wiek;
+
+    @FXML
+    private Button zapisz;
+
 }
